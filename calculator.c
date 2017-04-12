196b731028761aeb65d0d885fe97dc1a3171e406
Jakub Zich <xzichj00@stud.fit.vutbr.cz>
2017-04-12 19:22:32 +0200

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,52 @@
+#include "calculator.h"
+#include <math.h>
+#include <float.h>
+
+int error_ret;
+
+double ADD(double x, double y){
+    return 0;
+}
+
+double SUB(double x, double y){
+    return 0;
+}
+
+
+double MUL(double x, double y){
+    return 0;
+}
+
+
+double DIV(double x, double y){
+    return 0;
+}
+
+
+double FCT(int n){
+    return 0;
+}
+
+
+double POW(double x, int n){
+    return 0;
+}
+
+
+double SQRT(double x, int n){
+    return 0;
+}
+
+
+double ABSOL(double x){
+    return 0;
+}
+
+
+
+
+
+
+
+
+
