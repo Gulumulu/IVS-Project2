1fcf473b601658bfdef6e4c67770f38161f95f65
Jakub Zich <xzichj00@stud.fit.vutbr.cz>
2017-04-12 18:16:21 +0200

    Add files via upload

---
+++ calculator.h
@@ -0,0 +1,28 @@
+#ifndef calculator_h
+#define calculator_h
+
+
+extern int error_ret;
+
+double ADD(double x, double y);
+
+double SUB(double x, double y);
+
+double MUL(double x, double y);
+
+double DIV(double x, double y);
+
+double FCT(int n);
+
+double POW(double x, int n);
+
+double SQRT(double x, int n);
+
+double ABSOL(double x);                                                                                    \
+#endif
+
+
+
+
+
+
