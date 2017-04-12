e7c8cfe8a5718b39ada6014074481713d8c00a44
Jakub Zich <xzichj00@stud.fit.vutbr.cz>
2017-04-12 19:24:49 +0200

    Add files via upload

--- calculator.c
+++ calculator.c
@@ -5,40 +5,48 @@
 int error_ret;
 
 double ADD(double x, double y){
+    error_ret=1;
     return 0;
 }
 
 double SUB(double x, double y){
+    error_ret=1;
     return 0;
 }
 
 
 double MUL(double x, double y){
+    error_ret=1;
     return 0;
 }
 
 
 double DIV(double x, double y){
+    error_ret=1;
     return 0;
 }
 
 
 double FCT(int n){
+    error_ret=1;
     return 0;
 }
 
 
 double POW(double x, int n){
+    error_ret=1;
     return 0;
 }
 
 
 double SQRT(double x, int n){
+    error_ret=1;
     return 0;
 }
 
 
 double ABSOL(double x){
+    error_ret=1;
     return 0;
 }
 
