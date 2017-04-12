e46f3be4e2795efcc376605a405b201cc41ede7d
Jakub Zich <xzichj00@stud.fit.vutbr.cz>
2017-04-12 18:13:09 +0200

    Add files via upload

---
+++ test_calculator.h
@@ -0,0 +1,52 @@
+#ifndef test_calculator.h
+#define test_calculator.h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+#include <math.h>
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+const double EPS = 1e-14;
+
+#define TEST_EQUAL_INT(name,value1,value2)                                                  \
+	{                                                                                       \
+        if (value1!=value2)                                                                 \
+            {                                                                               \
+            count_errors++;                                                                 \
+            printf(ANSI_COLOR_RED   "Failed test - %s, line: %d!",name, __LINE__);          \
+            printf(ANSI_COLOR_RESET "\n");                                                  \
+            }                                                                               \
+        else                                                                                \
+            {                                                                               \
+            count_ok++;                                                                     \
+            printf(ANSI_COLOR_GREEN "Passed test - %s, line: %d!", name, __LINE__);         \
+            printf(ANSI_COLOR_RESET "\n");                                                  \
+            }                                                                               \
+	}
+
+
+#define TEST_EQUAL_FLOAT(name, value1, value2)                                              \
+    {                                                                                       \
+        if(fabs(value1-value2) >= EPS)                                                      \
+            {                                                                               \
+            count_errors++;                                                                 \
+            printf(ANSI_COLOR_RED   "Failed test - %s, line: %d!",name, __LINE__);          \
+            printf(ANSI_COLOR_RESET "\n");                                                  \
+            }                                                                               \
+        else                                                                                \
+            {                                                                               \
+            count_ok++;                                                                     \
+            printf(ANSI_COLOR_GREEN "Passed test - %s, line: %d!", name, __LINE__);         \
+            printf(ANSI_COLOR_RESET "\n");                                                  \
+            }                                                                               \
+    }                                                                                       \
+#endif
+
+
+
+
+
+
