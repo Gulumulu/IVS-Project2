a07a380ecc3f5b8a08f9bd595d9f2e6a23297112
Gabriel Quirschfeld <xquirs00@stud.fit.vutbr.cz>
2017-04-22 18:23:12 +0200

    Add files via upload

---
+++ src/main_calc/calculator.c
@@ -0,0 +1,86 @@
+#include "calculator.h"
+#include <math.h>
+#include <float.h>
+
+int error_ret;
+int error_div_zero;
+const double DEPS = 1e-14;
+
+double ADD(double x, double y){
+    error_ret=0;
+    return x+y;
+}
+
+double SUB(double x, double y){
+    error_ret=0;
+    return x-y;
+}
+
+double MUL(double x, double y){
+    error_ret=0;
+    return x*y;
+}
+
+double DIV(double x, double y){
+    if(y==0){
+        error_div_zero=1;
+        error_ret=1;
+        return 0;
+    }
+    else{
+        error_div_zero=0;
+        error_ret=0;
+        return x/y;
+    }
+}
+
+
+int FCT(unsigned long long int n){
+    int pom=1;
+    unsigned long long int tmp=n;
+    while(tmp>0){
+        pom=pom*tmp;
+        tmp--;
+        }
+    error_ret=0;
+    return pom;
+}
+
+
+double POW(double x, int n){
+    double pom=x;
+    for(int i=1; i<n; i++){
+        pom=pom*x;
+    }
+    error_ret=0;
+    return pom;
+}
+
+
+double SQRT(double x, int n) {
+	double result = 1.0;
+	double tmp = 1.0;
+	if (n < 1 || (x < 0 && !(n & 1))) {
+		error_ret = 1;
+		return 0.0 / 0.0;
+	}
+	do {
+		tmp = (x / POW(result, n - 1) - result) / n;
+		result += tmp;
+	} while (tmp >= DEPS * 10 || tmp <= -DEPS * 10);
+	error_ret = 0;
+	return result;
+}
+
+
+double ABSOL(double x){
+    if(x<0){
+        error_ret=0;
+        return -x;
+    }
+    else {
+        error_ret=0;
+        return x;
+    }
+}
+
---
+++ src/main_calc/callbacks.h
@@ -0,0 +1,76 @@
+#include <gtk/gtk.h>
+#ifndef CALLBACKS_H
+#define CALLBACKS_H
+
+GtkTextView *widg_2;
+GtkTextView *widg_1;
+
+gchar* get_whole_text(GtkTextBuffer *buffer);
+
+void window1_destroy();
+
+
+void one_clicked();
+
+
+void two_clicked();
+
+
+void three_clicked();
+
+
+void four_clicked();
+
+
+void five_clicked();
+
+
+void six_clicked();
+
+
+void seven_clicked();
+
+
+void eight_clicked();
+
+
+void nine_clicked();
+
+
+void zero_clicked();
+
+
+void add_clicked();
+
+
+void subtract_clicked();
+
+
+void square_clicked();
+
+
+void power_clicked();
+
+
+void faktorial_clicked();
+
+
+void mod_clicked();
+
+
+void multiply_clicked();
+
+
+void dot_clicked();
+
+
+void clear_clicked();
+
+
+void equal_clicked();
+
+
+void absclicked();
+
+#endif
+
